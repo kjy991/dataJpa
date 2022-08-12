2c3adaeda9fb2c3e2dd1dbf869ebb6a960c456cb
엽킹 <kim91star2@naver.com>
2022-08-13 00:58:23 +0900

    공통 인터페이스 적용

--- src/test/java/com/example/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/datajpa/repository/MemberRepositoryTest.java
@@ -9,7 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -30,11 +33,43 @@ public class MemberRepositoryTest {
 
         Assertions.assertThat(saveMember.getId()).isEqualTo(findMember.getId());
         Assertions.assertThat(saveMember.getUserName()).isEqualTo(findMember.getUserName());
-
         Assertions.assertThat(saveMember).isEqualTo(findMember);
 
+    }
 
+
+    @Test
+    void basicCRUD(){
+        Member memberA = Member.builder()
+                .userName("memberA")
+                .build();
+        Member memberB = Member.builder()
+                .userName("memberB")
+                .build();
+
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        Member findMemberA = memberRepository.findById(memberA.getId()).get();
+        Member findMemberB = memberRepository.findById(memberB.getId()).get();
+        assertThat(findMemberA).isEqualTo(memberA);
+        assertThat(findMemberB).isEqualTo(memberB);
+
+        findMemberA.setUserName("change MemberA");
+
+
+        List<Member> memberList = memberRepository.findAll();
+        assertThat(memberList.size()).isEqualTo(2);
+
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberRepository.delete(memberA);
+        memberRepository.delete(memberB);
+        long deleteCount = memberRepository.count();
+        assertThat(deleteCount).isEqualTo(0);
 
     }
+
 
 }

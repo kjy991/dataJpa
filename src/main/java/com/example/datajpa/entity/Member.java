60da73b6615b1a6885df69b87c2c8dbcac1a173c
엽킹 <kim91star2@naver.com>
2022-08-16 16:58:53 +0900

    @EntityGraph

--- src/main/java/com/example/datajpa/entity/Member.java
+++ src/main/java/com/example/datajpa/entity/Member.java
@@ -1,6 +1,7 @@
 package com.example.datajpa.entity;
 
 import lombok.*;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import javax.persistence.*;
 
@@ -32,5 +33,11 @@ public class Member {
     public Member(String username, int age) {
         this.username = username;
         this.age = age;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        this.team = team;
     }
 }
